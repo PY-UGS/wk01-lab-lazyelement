fbe64f81d83c8af5311d343a34dc1de4b3a15629
lazyElement <92869322+lazyelement@users.noreply.github.com>
2022-01-25 16:11:39 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,28 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello my name is Nadiah");
-        String module = "CSC1009";
-        switch (module) {
+        // 2(a) Example 1:
+        System.out.println("Hello I am Nadiah :)! \n");
+
+        // 2(b) Example 3:
+        String modules = "CSC1009";
+        switch (modules) {
+            case "CSC1006":
+                System.out.println("Mathematics 2");
+                break;
+            case "CSC1007":
+                System.out.println("Operating System");
+                break;
+            case "CSC1008":
+                System.out.println("Data Structure and Algortihms");
+                break;
+            case "CSC1009":
+                System.out.println("Object Oriented Programming");
+                break;
             case "CSC1010":
                 System.out.println("Computer Networks");
                 break;
-            case "CSC1008":
-                System.out.println("Data Structures and Algorithms");
-                break;
-            case "CSC1007":
-                System.out.println("Opearing Systems");
-                break;
-            case "CSC1009":
-                System.out.println("Object-Oriented Programming");
-                break;
             default:
-                System.out.println("Unknown module");
+                System.out.println("Unknown Module");
                 break;
         }
-        System.out.println("After switch");
-        for (int e = 120; e < 60; e--) {
-            System.out.println(" Value of e : " + e);
-            System.out.println("\n");
+        // System.out.println("After Switch \n");
+
+        // 2(c) Example 4:
+        for (int x = 120; x > 65; x--) {
+            // System.out.println("\n");
+            if (x % 2 != 0) {
+                System.out.println("Value of x: " + x);
+            }
+
         }
     }
 }
